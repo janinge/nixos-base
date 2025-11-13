65211dc99a0cdb399d59c4639ddfe73de39f3919
Jan Inge Sande <j@ninge.no>
2025-11-13 21:40:24 +0100

    Added SystemD unit for owntone

--- modules/sound-server.nix
+++ modules/sound-server.nix
@@ -3,7 +3,7 @@
 {
   security.rtkit.enable = true;
 
-  hardware.pulseaudio.enable = false;
+  services.pulseaudio.enable = false;
 
   services.pipewire = {
     enable = true;
@@ -17,7 +17,7 @@
 
   services.shairport-sync = {
     enable = true;
-    package = pkgs.shairport-sync-full; # includes AirPlay 2, PipeWire, etc., on recent Nixpkgs
+    package = pkgs.shairport-sync-airplay2;
 
     settings = {
       general = {
@@ -34,27 +34,90 @@
     };
   };
 
-  services.owntone = {
-    enable = true;
+  users.users.owntone = {
+    isSystemUser = true;
+    group = "owntone";
+    extraGroups = [ "audio" "pipewire" ];
+    description = "OwnTone service user";
+  };
 
-    config = {
-      "websocket" = {
-        "enabled" = true;
-      };
+  users.groups.owntone = {};
 
-      "library" = {
-        "name" = "Hiss";
-      };
+  environment.etc."owntone.conf".text = ''
+    general {
+      uid = "owntone"
+      db_path = "/var/lib/owntone/cache.db"
+      logfile = "/var/log/owntone.log"
+      loglevel = "log"
+    }
 
-      "output" = {
-        "pa" = {
-          "enabled" = true;
-        };
-      };
+    library {
+      name = "Hiss"
+      directories = { "/srv/music" }
+      follow_symlinks = true
+    }
 
-      "raop" = {
-        "enabled" = true;
-      };
+    audio {
+      nickname = "Computer"
+      type = "pulseaudio"
+      server = "127.0.0.1"
+    }
+
+    airplay_shared {
+      # Control all AirPlay devices
+      control_all_outputs = false
+    }
+
+    # DAAP/iTunes library sharing
+    library {
+      port = 3689
+      name = "Hiss"
+    }
+
+    mpd {
+      port = 6600
+      clear_queue_on_stop_disable = false
+    }
+  '';
+
+  systemd.tmpfiles.rules = [
+    "d /var/lib/owntone 0750 owntone owntone -"
+    "d /var/cache/owntone 0750 owntone owntone -"
+    "d /var/log 0755 root root -"
+    "f /var/log/owntone.log 0640 owntone owntone -"
+  ];
+
+  systemd.services.owntone = {
+    description = "OwnTone media server (forked-daapd)";
+    after = [ "network.target" "sound.target" "pipewire.service" ];
+    wants = [ "avahi-daemon.service" ];
+    wantedBy = [ "multi-user.target" ];
+
+    serviceConfig = {
+      Type = "simple";
+      ExecStart = "${pkgs.owntone}/bin/owntone -f -c /etc/owntone.conf";
+      User = "owntone";
+      Group = "owntone";
+      SupplementaryGroups = [ "audio" "pipewire" ];
+
+      NoNewPrivileges = true;
+      PrivateTmp = true;
+      ProtectSystem = "strict";
+      ProtectHome = true;
+      ReadWritePaths = [ "/var/lib/owntone" "/var/cache/owntone" "/var/log" ];
+
+      BindReadOnlyPaths = [ "/srv/music" ];
+
+      RuntimeDirectory = "owntone";
+      RuntimeDirectoryMode = "0755";
+
+      Restart = "on-failure";
+      RestartSec = "10s";
+    };
+
+    environment = {
+      # Ensure PipeWire socket is accessible
+      PIPEWIRE_RUNTIME_DIR = "/run/pipewire";
     };
   };
 
@@ -63,5 +126,6 @@
     qjackctl
     zita-njbridge
     alsa-utils
+    owntone
   ];
 }
