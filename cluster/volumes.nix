31b9efc16695dcf080fa74c8b19071e372cdddd9
Jan Inge Sande <j@ninge.no>
2026-02-21 22:23:30 +0100

    Renamed p2p volumes

--- cluster/volumes.nix
+++ cluster/volumes.nix
@@ -1,15 +1,15 @@
 # Nomad host volumes shared across all clients.
 # Per-host volumes can be added alongside these in each host file.
 {
-  "seaweedfs" = {
-    path = "/mnt/seaweedfs";
-    readOnly = false;
-    createDir = false;
-  };
   "local-stalwart" = {
     path = "/var/lib/nomad-volumes/stalwart";
     readOnly = false;
     createDir = true;
+  };
+  "weed-p2p" = {
+    path = "/mnt/seaweedfs/p2p";
+    readOnly = false;
+    createDir = false;
   };
   "local-p2p" = {
     path = "/var/lib/nomad-volumes/p2p";
